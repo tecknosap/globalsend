38f4220a22759608a6e9f20cd2ab3e5a0bc8e888
Mahad Mohamed <tecknosap@gmail.com>
2026-02-03 19:16:35 +0000

    Initial commit

--- terraform/modules/app-service/main.tf
+++ terraform/modules/app-service/main.tf
@@ -1,16 +1,24 @@
-
+# ---------------------------
+# App Service Plan
+# ---------------------------
+# This resource defines the App Service Plan for the Linux Web App. 
+# The App Service Plan determines the pricing tier, instance size, and scaling options.
 resource "azurerm_app_service_plan" "globalsend_app_service_plan" {
-  name = var.app_service_plan_name
+  name                = var.app_service_plan_name
   resource_group_name = var.resource_group_name
-  location = var.location
-    sku {
-    tier = "Standard"
-    size = "B1"
+  location            = var.location
+
+  sku {
+    tier = "Standard"  # Tier defines the pricing and features (Free, Basic, Standard, Premium)
+    size = "B1"        # Instance size; B1 is a small instance suitable for low traffic apps
   }
-  
 }
 
-
+# ---------------------------
+# Linux Web App
+# ---------------------------
+# This resource deploys a Linux-based Web App to Azure.
+# The app is linked to the App Service Plan created above and runs from a zip package.
 resource "azurerm_linux_web_app" "globalsend_app_service" {
   name                = var.app_service_name
   resource_group_name = var.resource_group_name
@@ -18,19 +26,20 @@ resource "azurerm_linux_web_app" "globalsend_app_service" {
   service_plan_id     = azurerm_app_service_plan.globalsend_app_service_plan.id
 
   site_config {
-    default_documents = ["index.html"]
+    default_documents = ["index.html"]  # Sets the default landing page for the app
   }
 
   app_settings = {
-    WEBSITE_RUN_FROM_PACKAGE = var.zip_blob_url
+    WEBSITE_RUN_FROM_PACKAGE = var.zip_blob_url  # Configures the app to run from a zip deployment package
   }
-
 }
 
-# ---------------------------
-# Output Web App URL
-# ---------------------------
-output "webapp_url" {
-  value       = "https://${azurerm_linux_web_app.globalsend_app_service.default_hostname}"
-  description = "The URL of the deployed Web App"
-}
+# # ---------------------------
+# # Output Web App URL
+# # ---------------------------
+# # Outputs the fully qualified domain name of the deployed web app.
+# # Useful for referencing the app URL in other resources or modules.
+# output "webapp_url" {
+#   value       = "https://${azurerm_linux_web_app.globalsend_app_service.default_hostname}"
+#   description = "The URL of the deployed Web App"
+# }
